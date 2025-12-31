0f2011fa2c04fcff65c9597b9712e20fb7061a00
PikemanCitadel <306958964+PikemanCitadel@users.noreply.github.com>
2025-12-31 18:39:01 +0300

    initial setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="latencyhist",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "click>=8.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "latencyhist=latencyhist.cli:main",
+        ],
+    },
+)
