fc44117f4c3bbdbb0de2b3ee7ad3acbff65a5a29
PikemanCitadel <306958964+PikemanCitadel@users.noreply.github.com>
2026-07-14 11:49:23 +0300

    test edge cases and empty lines

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -10,3 +10,14 @@ def test_custom_regex():
     line = "LATENCY=150ms"
     parser = LogParser(regex=r"LATENCY=(\d+)ms")
     assert parser.parse(line) == 150.0
+
+def test_malformed_line():
+    parser = LogParser(fmt="nginx")
+    assert parser.parse("garbage data") is None
+    assert parser.parse("") is None
+
+def test_comma_decimal():
+    # found this in some weird legacy app logs
+    line = 'request_time=0,501'
+    parser = LogParser(regex=r'request_time=(\d+[,.]\d+)')
+    assert parser.parse(line) == 0.501
